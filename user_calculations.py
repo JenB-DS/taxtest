59c6c91f01da4ccaa7124544e2c20ed2e6ef07c4
Jen Cheshire <jen.cheshire@ons.gov.uk>
2023-02-23 14:16:34 +0000

    format to function

--- user_calculations.py
+++ user_calculations.py
@@ -6,16 +6,17 @@ finance_cost_total = float(input("Total finance costs: "))
 division = int(input("How many people is this divided by: "))
 
 
+def calc_taxtest(income_total, deduction_total, finance_cost_total, division):
+    income_totalpp = round(income_total/division, 2)
+    deduction_totalpp = round(deduction_total/division, 2)
+    finance_cost_totalpp = round(finance_cost_total/division, 2)
 
-income_totalpp = round(income_total/division, 2)
-deduction_totalpp = round(deduction_total/division, 2)
-finance_cost_totalpp = round(finance_cost_total/division, 2)
-
-rental_profit = round(income_totalpp - deduction_totalpp, 2)
+    rental_profit = round(income_totalpp - deduction_totalpp, 2)
     # tax calculated from profit
-income_tax_profit = round(rental_profit * 0.2, 2)
+    income_tax_profit = round(rental_profit * 0.2, 2)
     # can deduct a further 20% of mortgage interest costs
-tax_credit = round(finance_cost_totalpp * 0.2, 2)
-tax_owedpp = round(income_tax_profit - tax_credit, 2)
+    tax_credit = round(finance_cost_totalpp * 0.2, 2)
+    tax_owedpp = round(income_tax_profit - tax_credit, 2)
+    return tax_owedpp
 
-print(tax_owedpp)
+tax_owedpp = calc_taxtest(income_total, deduction_total, finance_cost_total, division)
